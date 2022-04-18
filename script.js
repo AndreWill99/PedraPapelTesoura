d04e6d6508505d3dcfb539c5e9f2263f5c8b1033
ANDRE WILLIAM <89886682+AndreWill99@users.noreply.github.com>
2022-04-18 23:15:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var plyerScore = 0
+var iaScore = 0
+
+
+function play(player){
+  var ia =Math.floor( Math.random()*3)
+  var ganhador =""
+  //Ia ganha
+  if((ia== 0 && player ==2 ) ||(ia == 1 && player ==0) || (ia == 2 && player == 1)){
+    ganhador = "IA"
+    iaScore++
+    
+  }else if(ia == player){
+ganhador = "Ninguem"    
+  }else{
+    ganhador = "PLAYER"
+    playerScore++
+  }
+  var p1 = document.getElementById("winner")
+  p1.innerHTML = ganhador + "Ganhou!"
+
+  var p2 = document.getElementById("score")
+  p2.innerHTML = "PLAYER:" + playerScore + 'X' + iaScore +" :IA"
+  
+}
